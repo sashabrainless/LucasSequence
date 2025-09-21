305c283541d7d7fc3e4c0132c9ad8de954c0ed99
Sasha <sashabrainless@gmail.com>
2025-09-21 16:10:17 +0300

    Add files via upload

---
+++ LucasModule/src/main/java/org/lucassequence/LucasSequenceClass.java
@@ -0,0 +1,58 @@
+package org.lucassequence;
+
+public class LucasSequenceClass {
+    long[] lucasnumbers;
+
+    LucasSequenceClass () { //варіант порожнього конструктора
+    }
+
+    LucasSequenceClass (int n) { //варіант конструктора з параметрами
+        initNumbers(n);
+    }
+
+    void initNumbers(int count) { //ініціалізація ряду Люка
+        lucasnumbers = new long [count];
+        lucasnumbers[0] = 2;
+        lucasnumbers[1] = 1;
+        for (int i = 2; i < count; i++) {
+            lucasnumbers[i] = lucasnumbers[i-2] + lucasnumbers[i-1];
+        }
+    }
+    void printNumbers() { //вивід чисел
+        for (int i = 0; i < lucasnumbers.length; i++) {
+            System.out.println("luke[" + i + "] = " + lucasnumbers[i]);
+        }
+    }
+
+    void squaredNumbersPlusOne() { //перевірка чисел на відповідність формулі (w^2)+1
+        for (int i = 0; i < lucasnumbers.length; i++) {
+            double w = Math.sqrt(lucasnumbers[i] - 1);
+            if (w - Math.floor(w) == 0) {
+                System.out.printf("luke[%d] = %d, w = %.0f%n", i, lucasnumbers[i], w);
+            }
+        }
+    }
+
+    void reutrnIndex(long num) { //додаткова функція знаходження індексу числа
+        boolean isfound = false;
+        for (int i = 0; i < lucasnumbers.length; i++) {
+            if (lucasnumbers[i] == num) {
+                System.out.println("luke = " + lucasnumbers[i] + ", index = " + i);
+                isfound = true;
+                return;
+            }
+        }
+        if (!isfound) {
+            System.out.println("Такого числа немає!");
+        }
+    }
+
+    void findByIndex(int index) { //додаткова функція знаходження числа за індексом
+        if (!(index > lucasnumbers.length) && !(index < 0)) {
+            System.out.println("luke[" + index + "] = " + lucasnumbers[index]);
+        }
+        else {
+            System.out.println("Немає числа з таким індексом!");
+        }
+    }
+}
---
+++ LucasModule/src/main/java/org/lucassequence/Main.java
@@ -0,0 +1,50 @@
+package org.lucassequence;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            System.out.println("Лаборнаторна робота №1. Основи Java\n" +
+                    "Виконала Жандик О. А. ОІ-11сп, варіант 8\n" +
+                    "------------------------------------------------------------------");
+
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Введіть кількість N чисел в ряду Люка: ");
+            int n = scanner.nextInt();
+
+            if (n <= 90) {
+                LucasSequenceClass luke = new LucasSequenceClass(n);
+                System.out.println("----------------------------Числа Люка----------------------------");
+                luke.printNumbers();
+                System.out.println("------------------------------------------------------------------");
+                System.out.print("1 - Числа ряду Люка, що є на 1 більшими за квадрат числа w\n" +
+                        "2 - Пошук індекса за числом\n" +
+                        "3 - Пошук числа за індексом\n" +
+                        "Оберіть: ");
+                int var = scanner.nextInt();
+                System.out.println("------------------------------------------------------------------");
+                switch (var) {
+                    case 1:
+                        luke.squaredNumbersPlusOne();
+                        break;
+                    case 2:
+                        System.out.println("Введіть число: ");
+                        int number = scanner.nextInt();
+                        luke.reutrnIndex(number);
+                        break;
+                    case 3:
+                        System.out.println("Введіть індекс: ");
+                        int index = scanner.nextInt();
+                        luke.findByIndex(index);
+                        break;
+                }
+            }
+            else {
+                System.out.println("Максимальне значення N = 90, так як максимальне значення long змінної = 9,223,372,036,854,775,807");
+            }
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+}
